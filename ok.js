766f3afb13e1c8a27ca3a7a207779ec86a04a54f
rjrobins16 <rjrobins16@gmail.com>
2019-08-08 16:35:30 -0500

    classwork

---
+++ ok.js
@@ -0,0 +1,42 @@
+
+//W1
+//Print -10 through 5 using a counter and while loop.
+
+var count = -10;
+while (count < 5)
+{
+    console.log(count);
+    count=count+1;
+
+}
+    console.log(count);
+
+
+//Write a While loop that counts backwards from 10 to 0
+//Log the current count at the end of each iteration EXCEPT if the current count is ```5```
+
+var count1 = 10;
+while (count1 >= 1)
+{
+    console.log(count1);
+    count1=count1-1;
+}
+console.log(count1);
+
+
+//Asks the user ```Enter a word. Enter 'q' to quit.``` Create a while loop that will not quit until they enter 'q'.
+
+var word = prompt("Enter a word.");
+var password = "q";
+var entry =
+
+{
+    if (entry ===q)
+    {
+        alert("Password correct");
+        isvalid = true
+    }
+    else {
+        var1 prompt=("Enter a word.")
+    }
+    }
